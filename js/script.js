29971aa54ddc49f758520715dd2670bec59ab652
Fenisa <t108ac1023@ntut.org.tw>
2024-02-05 01:39:20 +0800

    message

--- js/script.js
+++ js/script.js
@@ -30,17 +30,21 @@ $(document).ready(function() {
     $(window).scroll( function(){
         $('.fadein').each( function(i){
             
-            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight()/3;
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             
             if( bottom_of_window > bottom_of_element ){
-                $(this).animate({'opacity':'1'},600);
+                $(this).animate({
+                    opacity:'1',
+                    top:'0px',
+                },600);
             }
             
         }); 
     });
 
 });
+
 
 
   
