6a9964c0d4b5dcc5120e7d2361fecae7d174cc63
Fenisa <t108ac1023@ntut.org.tw>
2024-03-04 02:49:07 +0800

    message

--- js/script.js
+++ js/script.js
@@ -1,3 +1,41 @@
+
+
+// jquery
+$(document).ready(function() {
+    $(window).scroll( function(){
+        $('.fadein').each( function(i){
+            
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight()/3;
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            if( bottom_of_window > bottom_of_element ){
+                $(this).animate({
+                    opacity:'1',
+                    top:'0px',
+                },600);
+            }
+            
+        }); 
+    });
+});
+
+$(function() {
+    /*按下*/
+    $('#gotop').click(function(){
+        $('html,body').animate({ scrollTop: 0 }, 'slow');
+        return false;
+    });
+    
+    /*出現 */
+    $(window).scroll(function() {
+        if ( $(this).scrollTop() > 600){
+            $('#gotop').fadeIn();
+        } else {
+            $('#gotop').fadeOut();
+        }
+    });
+});
+
 // lottie
 LottieInteractivity.create({
     player:'#clickPlayer',
@@ -24,27 +62,6 @@ LottieInteractivity.create({
         }
     ]
 });
-
-// jquery
-$(document).ready(function() {
-    $(window).scroll( function(){
-        $('.fadein').each( function(i){
-            
-            var bottom_of_element = $(this).offset().top + $(this).outerHeight()/3;
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
-            if( bottom_of_window > bottom_of_element ){
-                $(this).animate({
-                    opacity:'1',
-                    top:'0px',
-                },600);
-            }
-            
-        }); 
-    });
-
-});
-
 
 
   
